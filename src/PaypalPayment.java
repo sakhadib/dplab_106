ec845cb9da4201287207c1c598a8905098f458d5
sakhadib <adibsakhawat@iut-dhaka.edu>
2024-10-19 12:42:48 +0600

    lab_3

--- src/PaypalPayment.java
+++
@@ -1,6 +0,0 @@
-public class PaypalPayment implements PaymentMethod{
-    @Override
-    public void pay(int dollars) {
-        System.out.println("Paying " + dollars + " using Paypal.");
-    }
-}
--- src/VEHICLETYPE.java
+++
@@ -1,3 +0,0 @@
-public enum VEHICLETYPE {
-    CAR, BIKE, LUXURYCAR
-}
